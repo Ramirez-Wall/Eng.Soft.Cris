77ccf6008b980b548926ee9b70c01a9d4f9ebdb3
LuizCPina <luizricopina@hotmail.com>
2025-08-18 23:26:23 -0300

    commit do bug

--- poetry-demo/main.py
+++ poetry-demo/main.py
@@ -5,7 +5,7 @@ import emoji
 init(autoreset=True)
 
 # Função para mostrar mensagens coloridas com emojis
-def log(message: str, level: str = "info"):
+def funcao(message: str, level: str = "info"):
     if level == "success":
         print(Fore.GREEN + emoji.emojize(message, language="alias"))
     elif level == "info":
@@ -16,8 +16,9 @@ def log(message: str, level: str = "info"):
         print(Style.RESET_ALL + message)
 
 # Testando a função
-log("Olá! :smile:", "success")
-log("sucesso.", "success")
-log("Hoje é um ótimo dia para programar! :laptop:", "info")
-log("Erro detectado! :x:", "error")
-log("Mensagem normal sem cor ou emoji.")
+funcao("Olá! :smile:", "success")
+funcao("sucesso.", "success")
+funcao("Hoje é um ótimo dia para programar! :laptop:", "info")
+funcao("Erro detectado! :x:", "error")
+funcao("Mensagem normal sem cor ou emoji.")
+funcao("Mensagem normal sem cor ou emoji, só para criar o problema de merge.")
